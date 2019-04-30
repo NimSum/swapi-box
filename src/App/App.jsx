cd6559523dc8ac9ed2c61cde70196a196e3bea40
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-30 17:08:17 -0600

    Fetch all films, then select a random one

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    <p>HIIII</p>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedMovie: '',
+      loading: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/films')
+      .then(response => response.json())
+      .then(result => 
+          this.setState({ selectedMovie: result.results
+            .sort(() => Math.random() - 0.5).pop()
+            }))
+      .catch(error => console.log(error) )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>HI</p>
+      </div>
+    )
+  }
 }
+
+
+
 
 export default App;
