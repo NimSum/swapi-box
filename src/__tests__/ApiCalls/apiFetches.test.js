f05e379e93e2ddceee3bcf50f4198f1fbdb76bb3
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-02 18:50:10 -0600

    Re-factor mock window fetch method, test fetchCategory method

--- src/__tests__/ApiCalls/apiFetches.test.js
+++ src/__tests__/ApiCalls/apiFetches.test.js
@@ -1,30 +1,73 @@
-import { fetchRandomMovie } from "../../ApiCalls/apiFetches";
+import { fetchRandomMovie, fetchCategory } from "../../ApiCalls/apiFetches";
 
-describe('Api Fetches', () => {
-  const mockResponse = { movie: 'The Force Awakens' };
-  const mockFilmUrl = 'https://swapi.co/api/films/1'
+describe('Api Fetches', () => {  
+  const mockReject = jest.fn().mockImplementation(() => 
+    Promise.resolve({ ok: false }));
 
-  window.fetch = jest.fn().mockImplementation(()=> 
+  const mockResolve = (response) => jest.fn().mockImplementation(() => 
     Promise.resolve({
       ok: true,
-      json: () => Promise.resolve(mockResponse)
-    }))
+      json: () => Promise.resolve(response)
+    }));
 
-  it('Should fetch with the correct params', () => {
-    fetchRandomMovie(1);
-    expect(window.fetch).toHaveBeenCalledWith(mockFilmUrl);
-  })
 
-  it('Should return parsed response if response "ok"', async () => {
-    const result = await fetchRandomMovie(1)
-    expect(result).toEqual(mockResponse);
-  })
+  describe('Fetch film', () => {
+    const mockFilmResponse = { film: 'The Force Awakens' };
+    const mockFilmUrl = 'https://swapi.co/api/films/1';
+    const mockFilmId = 1;
 
-  it('Should return an error if response fails', async () => {
-    window.fetch = jest.fn().mockImplementation(() => 
-    Promise.resolve({ ok: false }))
-    
-    await expect(fetchRandomMovie(1)).rejects.toEqual(Error('Error fetching movies'));
-  })
+    it('Should fetch with the correct params', () => {
+      window.fetch = mockResolve(mockFilmResponse);
 
+      fetchRandomMovie(mockFilmId);
+      expect(window.fetch).toHaveBeenCalledWith(mockFilmUrl);
+    })
+  
+    it('Should return parsed response if response "ok"', async () => {
+      window.fetch = mockResolve(mockFilmResponse);
+      const result = await fetchRandomMovie(mockFilmId)
+
+      expect(result).toEqual(mockFilmResponse);
+    })
+  
+    it('Should return an error if response fails', async () => {
+      window.fetch = mockReject;
+      
+      await expect(fetchRandomMovie(mockFilmId)).rejects.toEqual(Error('Error fetching movies'));
+    })
+  });
+
+  describe('Fetch category', () => {
+    const mockCategory = 'people';
+    const mockCategoryUrl = 'https://swapi.co/api/people/';
+    const mockCategoryResult = {
+      results: [
+        { name: "Luke Skywalker" },
+        { name: "Obi-wan Kenobi" } 
+      ] 
+    };
+
+    it('Should fetch using the correct parameter', () => {
+      window.fetch = mockResolve(mockCategoryResult);
+
+      fetchCategory(mockCategory);
+      expect(window.fetch).toHaveBeenCalledWith(mockCategoryUrl);
+    })
+
+    it('Should return result array if response "ok"', async () => {
+      window.fetch = mockResolve(mockCategoryResult);
+
+      const result = await fetchCategory(mockCategory);
+      expect(result).toEqual(mockCategoryResult.results);
+    })
+
+    it('Should return error if response NOT "ok"', async () => {
+      window.fetch = mockReject;
+
+      await expect(fetchCategory(mockCategory)).rejects.toEqual(Error('Error fetching category items'));
+    })
+  });
+
+  
+  
 })
