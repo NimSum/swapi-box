2c0004d0d94caf8c25d7d642c65737734a7e8fe4
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-05 14:39:19 -0600

    Updates

---
+++ precache-manifest.a9319f408498f24b4fdb963072f6545d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621a89dba4f0e4b554e8f05f4c667fa3",
+    "url": "/swapi-box/index.html"
+  },
+  {
+    "revision": "ea78fc5e50af98d86b90",
+    "url": "/swapi-box/static/css/main.0ada6921.chunk.css"
+  },
+  {
+    "revision": "585f3ed7b9e08ce78a30",
+    "url": "/swapi-box/static/js/2.e8dbd928.chunk.js"
+  },
+  {
+    "revision": "ea78fc5e50af98d86b90",
+    "url": "/swapi-box/static/js/main.6d1650d2.chunk.js"
+  },
+  {
+    "revision": "131d28aeaaf818238108",
+    "url": "/swapi-box/static/js/runtime~main.a3482073.js"
+  },
+  {
+    "revision": "4407be4a5fe37a4b6fff8025e83a626f",
+    "url": "/swapi-box/static/media/active-star.4407be4a.svg"
+  },
+  {
+    "revision": "060443cf1e0bd7c75fba87e473bbcd2a",
+    "url": "/swapi-box/static/media/bb8-body.060443cf.svg"
+  },
+  {
+    "revision": "73124d263cf4aef915280d8ec10b664b",
+    "url": "/swapi-box/static/media/bb8-head.73124d26.svg"
+  },
+  {
+    "revision": "b5539d880b28e4aa5b0d14f73d66cf50",
+    "url": "/swapi-box/static/media/character-bg.b5539d88.png"
+  },
+  {
+    "revision": "2e4838bd09b89fab57de95b5b8afe711",
+    "url": "/swapi-box/static/media/death-star.2e4838bd.svg"
+  },
+  {
+    "revision": "88b9c1f5d3e779eb7e6f353c1c851b33",
+    "url": "/swapi-box/static/media/empire.88b9c1f5.png"
+  },
+  {
+    "revision": "eb32f0e07387986facc2266c9a228c1d",
+    "url": "/swapi-box/static/media/inactive-star.eb32f0e0.svg"
+  },
+  {
+    "revision": "8c1230154170ae26bc78d61389ce1d50",
+    "url": "/swapi-box/static/media/planet-1.8c123015.png"
+  },
+  {
+    "revision": "8efd3468b3f55b6c9b0e6f87b58749b9",
+    "url": "/swapi-box/static/media/planet-2.8efd3468.png"
+  },
+  {
+    "revision": "04f4528e259466b742b2a742e0183802",
+    "url": "/swapi-box/static/media/planet-3.04f4528e.png"
+  },
+  {
+    "revision": "63fbdc29feb222effc6473c259886916",
+    "url": "/swapi-box/static/media/planet-4.63fbdc29.png"
+  },
+  {
+    "revision": "6c34ccb8a9e00dc4ace8c28881d33cee",
+    "url": "/swapi-box/static/media/planet-5.6c34ccb8.png"
+  },
+  {
+    "revision": "9cf527df856b98c1b28e5fa2db4e098b",
+    "url": "/swapi-box/static/media/planet-6.9cf527df.png"
+  },
+  {
+    "revision": "1807eb95d30db8c6e8c9f4793f7753e1",
+    "url": "/swapi-box/static/media/planet-7.1807eb95.png"
+  },
+  {
+    "revision": "e9f04552e71ac0ebd48cdca0bff56a0c",
+    "url": "/swapi-box/static/media/rebel-alliance.e9f04552.png"
+  },
+  {
+    "revision": "171057266971967defb395ce23f2bb9c",
+    "url": "/swapi-box/static/media/star-wars-title.17105726.png"
+  },
+  {
+    "revision": "e536237ca1a521c232b0f9e20158783a",
+    "url": "/swapi-box/static/media/stars-bg.e536237c.jpg"
+  },
+  {
+    "revision": "6027a8e6e29560e060539c672eb61fa8",
+    "url": "/swapi-box/static/media/tie-fighter.6027a8e6.svg"
+  },
+  {
+    "revision": "98a8b2f6cb1801f47b7ce0cd55ec013a",
+    "url": "/swapi-box/static/media/vader.98a8b2f6.svg"
+  },
+  {
+    "revision": "5229d72b4d7b90d36b89e9dd0b06ddc3",
+    "url": "/swapi-box/static/media/vehicle-bg.5229d72b.png"
+  }
+]);
